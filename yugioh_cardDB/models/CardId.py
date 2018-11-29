98270d6a6f0fffac138c389e2c0d04cc505b47a2
ss6987 <angel.blossom.vivid@gmail.com>
2018-11-29 13:57:24 +0900

    同じIDに複数のレアリティが存在する可能性があるため、CardId.rarityをForeignKeyからManyToManyFieldに変更

--- yugioh_cardDB/models/CardId.py
+++ yugioh_cardDB/models/CardId.py
@@ -4,7 +4,7 @@ from django.db import models
 class CardId(models.Model):
     card_id = models.CharField('card_id', max_length=20, primary_key=True)
     card_name = models.ForeignKey('Card', on_delete=models.CASCADE, related_name='card_id')
-    rarity = models.ForeignKey('Rarity', on_delete=models.CASCADE, related_name='card_id')
+    rarity = models.ManyToManyField('Rarity', related_name='card_id')
 
     def __str__(self):
         return self.card_id + ',' + self.card_name.card_name
