918ea503fc8032db2a6f35e09653f4e3a9d58495
ss6987 <angel.blossom.vivid@gmail.com>
2018-12-02 20:01:57 +0900

    遊戯王公式DBに接続する方法をseleniumからurllib3に変更 進行バー追加

--- yugioh_cardDB/management/commands/GetCardData.py
+++ yugioh_cardDB/management/commands/GetCardData.py
@@ -4,17 +4,22 @@ from yugioh_cardDB.management.commands.GetCardDetail import *
 from yugioh_cardDB.management.commands.ReadCardDetail import *
 from yugioh_cardDB.management.commands.RegistrationLinkMarker import *
 from yugioh_cardDB.models import *
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+
+urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # Card.objects.all().delete()
+        Card.objects.all().delete()
+
         getCardHTML()
-        print("URLリスト取得完了")
+        print("カード詳細URL取得完了")
         getCardDetail()
         print("カード詳細取得完了")
         openFile()
-        print("カードデータ登録完了")
+        print("カード詳細登録完了")
         registrationLinkMarker()
         print("リンクマーカー登録完了")
         print("ALLコンプリート")
--- yugioh_cardDB/management/commands/GetCardDetail.py
+++ yugioh_cardDB/management/commands/GetCardDetail.py
@@ -1,23 +1,27 @@
 from time import sleep
 import os
-from yugioh_cardDB.management.commands.SettingDriver import settingDriver
+import urllib3
+from tqdm import tqdm
 
 
 def getCardDetail():
     file = open("yugioh_cardDB/texts/search_result/search_result.txt", "r", encoding="utf-8")
+    max_line = len(file.readlines())
+    file.seek(0)
+    bar = tqdm(total=max_line,position=0)
     line = file.readline()
     if not os.path.isdir("yugioh_cardDB/texts/card_detail_html"):
         os.mkdir("yugioh_cardDB/texts/card_detail_html")
     while line:
         name, url = line.strip().split(",")
-        name = name.replace("&44;", ",")
-        print(name)
         cid = url[url.index("cid=") + 4:]
-        driver = settingDriver()
-        driver.get("https://www.db.yugioh-card.com" + url + "&request_locale=ja")
+        http = urllib3.PoolManager()
+        request = http.request('GET', "https://www.db.yugioh-card.com" + url + "&request_locale=ja")
         html_file = open("yugioh_cardDB/texts/card_detail_html/" + cid + ".txt", "w", encoding="utf-8")
-        html_file.write(driver.page_source)
+        html_file.write(request.data.decode("utf-8"))
         html_file.close()
-        sleep(2)
         line = file.readline()
+        bar.update(1)
+        sleep(1)
+    bar.close()
     file.close()
