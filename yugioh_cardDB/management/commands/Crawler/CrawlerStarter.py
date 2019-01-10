a26cdab13354ef2eee0319ebdc2090d36412867d
ss6987 <angel.blossom.vivid@gmail.com>
2019-01-10 12:33:03 +0900

    RuntimeError改善のため、スレッド数を調整

--- yugioh_cardDB/management/commands/Crawler/CrawlerStarter.py
+++ yugioh_cardDB/management/commands/Crawler/CrawlerStarter.py
@@ -7,8 +7,8 @@ def crawlerStarter():
     # ShopURL.objects.all().delete()
     shop_all = SearchPage.objects.all()
     # priceSearch(shop_all.filter(page_name="宝島").first())
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=32)
-    executor.submit(priceSearch, shop_all.filter(page_name="宝島").first(), 0)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=8)
+    # executor.submit(priceSearch, shop_all.filter(page_name="宝島").first(), 0)
     # executor.submit(priceSearch, shop_all.filter(page_name="若院").first(), 0)
-    # for shop in shop_all:
-    #     executor.submit(priceSearch,shop)
+    for number, shop in enumerate(shop_all):
+        executor.submit(priceSearch, shop, number)
