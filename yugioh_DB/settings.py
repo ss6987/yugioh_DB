4fc9de406079bcc4d70f24c75207342d8f98fea8
ss6987 <angel.blossom.vivid@gmail.com>
2019-05-05 22:05:17 +0900

    DBをSQLiteからpostgresに変更

--- yugioh_DB/settings.py
+++ yugioh_DB/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .config import USERNAME,PASSWORD,IP
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,8 +80,12 @@ WSGI_APPLICATION = "yugioh_DB.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'yugioh_db',
+        'USER': USERNAME,
+        'PASSWORD': PASSWORD,
+        'HOST': IP,
+        'PORT': '',
     }
 }
 
