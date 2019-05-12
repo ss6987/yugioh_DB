ce80ecebb8aec81edf7446508f0166cf88f59bdf
ss6987 <angel.blossom.vivid@gmail.com>
2019-05-12 10:03:06 +0900

    ネットワークが落ちる問題を対策

--- yugioh_cardDB/management/commands/GetData/GetCardDetail.py
+++ yugioh_cardDB/management/commands/GetData/GetCardDetail.py
@@ -1,9 +1,16 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from bs4 import BeautifulSoup
 
 
-def getCardDetail(url,driver):
-
-    driver.get("https://www.db.yugioh-card.com" + url + "&request_locale=ja")
+def getCardDetail(url, driver):
+    flag = True
+    count = 0
+    while flag and count <= 10:
+        try:
+            driver.get("https://www.db.yugioh-card.com" + url + "&request_locale=ja")
+            flag = False
+        except WebDriverException as e:
+            count += 1
     soup = BeautifulSoup(driver.page_source, "html.parser")
     return soup
