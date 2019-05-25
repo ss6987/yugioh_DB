92cc8354a4114b65d1c6b15a900d92e8b9abffca
ss6987 <angel.blossom.vivid@gmail.com>
2019-05-25 20:22:24 +0900

    chromedriverのプロセスが大量発生する問題に対応

--- yugioh_cardDB/management/commands/GetData/GetCardHTML.py
+++ yugioh_cardDB/management/commands/GetData/GetCardHTML.py
@@ -6,7 +6,13 @@ from tqdm import tqdm
 
 options = Options()
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-setuid-sandbox")
+options.add_argument('--disable-gpu')
+options.add_argument('--lang=ja')
+options.add_argument('--blink-settings=imagesEnabled=false')
 driver = webdriver.Chrome(options=options)
+
 fields = {
     "ope": "1",
     "sess": "1",
